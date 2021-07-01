e694f396d332e34ceccef0440243aef2a5becadc
Mario Saliba <mario.saliba98@gmail.com>
2021-06-30 23:45:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const router = require("./router");
 
 const server = http.createServer(router);
 
-server.listen(3002, () => console.log(`Listening at http://localhost:3002`));
+server.listen(process.env.PORT || 3002, () => console.log(`Listening at http://localhost:3002`));
 
